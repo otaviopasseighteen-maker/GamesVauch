49f04c1e54970f8416d1d80b4d8cbda8b3de1f12
Otavio Pass <otaviopasseighteen@gmail.com>
2026-09-13 20:28:59 -0300

    feat: configure MySQL connection pool

---
+++ backend/src/config/database.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql2/promise';
+import { env } from './env.js';
+
+export const pool = mysql.createPool({
+  host: env.database.host,
+  port: env.database.port,
+  user: env.database.user,
+  password: env.database.password,
+  database: env.database.name,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
